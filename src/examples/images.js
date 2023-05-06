56da73b6151546f40ad8c80f98ea58dd84ef93cb
dennisison <dennismison@gmail.com>
2023-05-06 13:18:55 -0600

    completed images exercise

--- src/examples/images.js
+++ src/examples/images.js
@@ -1,19 +1,65 @@
 import React from "react";
 import styled from "styled-components";
-import big from "../assets/images/big.jpg";
 import { StaticImage } from "gatsby-plugin-image";
 const images = () => {
   return (
     <Wrappter>
-      <StaticImage src="../assets/images/big.jpg" alt="food" />
-      <h2>gatsby images</h2>
+      <article>
+        <h4>contrained / default</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="tracedSVG"
+          layout="constrained"
+          className="example-img"
+          as="section"
+        />
+      </article>
+      <article>
+        <h4>fixed</h4>
+        <StaticImage
+          src="../assets/images/recipe-1.jpeg"
+          alt="food"
+          placeholder="blurred"
+          layout="fixed"
+          width={200}
+          className="example-img"
+          as="div"
+        />
+        <article>
+          <h4>full width</h4>
+          <StaticImage
+            src="../assets/images/recipe-1.jpeg"
+            alt="food"
+            placeholder="dominantColor"
+            layout="fullWidth"
+            className="example-img"
+            as="article"
+          />
+        </article>
+      </article>
     </Wrappter>
   );
 };
 
 const Wrappter = styled.section`
-  img {
-    width: 200px;
+  width: 70vw;
+  margin: 0 auto;
+  display: grid;
+  text-align: center;
+  gap: 2rem;
+  article {
+    border: 2px solid red;
+  }
+  .example-img {
+    border-radius: 1rem;
+    height: 300px;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr 1fr;
+    .example-img {
+      height: 200px;
+    }
   }
 `;
 export default images;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import * as React from "react";
-import { Link } from "gatsby";
 import Layout from "../components/Layout";
-import Images from "../examples/images";
 
 export default function Home() {
   return (
     <Layout>
       <h1>Home Page</h1>
-      <Images />
     </Layout>
   );
 }
