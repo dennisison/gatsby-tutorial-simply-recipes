9dc50fac8bc2fe3feca7a456b71dbfde3fb672f8
dennisison <dennismison@gmail.com>
2023-05-12 12:34:27 -0600

    working on recipes list

---
+++ src/components/RecipesList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {Link} from 'gatsby'
+import { GatsbyImage } from "gatsby-plugin-image";
+
+const RecipesList = ({recipes=[]}) => {
+
+  return <div className="recipes-list">{
+    recipes.map((recipe)=>{
+      return <p>{recipe.title}</p>
+    })
+    }</div>;
+};
+
+export default RecipesList;
