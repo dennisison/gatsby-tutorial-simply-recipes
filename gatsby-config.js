b48fb415d41431d33b3d7646b0d911f80b604b97
dennisison <dennismison@gmail.com>
2023-05-24 05:47:03 -0600

    added fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,22 +1,19 @@
 /**
  * Configure your Gatsby site with this file.
  *
- * See: https://www.gatsbyjs.com/docs/reference/config-files/gatsby-config/
- */
-
-/**
- * @type {import('gatsby').GatsbyConfig}
+ * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
 });
 
 module.exports = {
+  /* Your site config here */
   siteMetadata: {
     title: "Simply Recipes",
     description: "Nice and clean recipes site",
-    author: "@johnsmilga",
-    person: { name: "john", age: 32 },
+    author: "@dennisison",
+    person: { name: "dennis", age: 43 },
     simpleData: ["item 1", "item 2"],
     complexData: [
       { name: "john", age: 32 },
@@ -31,9 +28,7 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        // The unique name for each instance
         name: `images`,
-        // Path to the directory
         path: `${__dirname}/src/assets/images`,
       },
     },
@@ -45,5 +40,23 @@ module.exports = {
         accessToken: process.env.CONTENTFUL_API_KEY,
       },
     },
+    {
+      resolve: `gatsby-plugin-webfonts`,
+      options: {
+        fonts: {
+          google: [
+            {
+              family: "Montserrat",
+              variants: ["400"],
+            },
+            {
+              family: "Inconsolata",
+              variants: ["400", "500", "600", "700"],
+            },
+          ],
+        },
+      },
+    },
+    `gatsby-plugin-react-helmet`,
   ],
 };
